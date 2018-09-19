7162157ac2cb5841d684b125e13708642173aa9a
TomZhu <zhuyurun1993@gmail.com>
2018-09-19 18:46:59 +0800

    add menus interface

--- src/seed/api/endpoints/menu.py
+++ src/seed/api/endpoints/menu.py
@@ -24,7 +24,8 @@ class Menu(RestfulBaseView):
         """
         query_session = self.session.query(self.model_class)
         menu_data = query_session.all()
-
+        menus = self._encode_menus(menu_data)
+        return self.response_json(self.HttpErrorCode.SUCCESS, data=menus)
 
     def post(self):
         """ 更新菜单结构
@@ -34,9 +35,23 @@ class Menu(RestfulBaseView):
         return self.response_json(self.HttpErrorCode.SUCCESS)
     
     def _encode_menus(self, menu_data):
-        menu_data = {'-'.join([row.parent_id, row.left_id]): row.row2dict() for row in menu_data}
-        menus = []
-        tree_stack = []
+        menu_data = {'-'.join([str(row.parent_id), str(row.left_id)]): row.row2dict() for row in menu_data}
+
+        menus = {'id': 0}
+        middle_menu = [menus]
+        while middle_menu:
+            current_menu = middle_menu.pop()
+            parent_id, left_id = current_menu['id'], 0
+
+            while True:
+                current_key = '-'.join([str(parent_id), str(left_id)])
+                if current_key not in menu_data:
+                    break
+                current_menu.setdefault('sub_menus', []).append(menu_data[current_key])
+                middle_menu.append(menu_data[current_key])
+                left_id = menu_data[current_key]['id']
+
+        return menus['sub_menus']
 
 
     def _decode_menus(self, menus, parent_id=0, left_id=0):
