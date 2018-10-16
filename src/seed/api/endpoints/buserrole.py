cd66cb670e5f01317f16871e8b1a66f68bcbbdcd
TomZhu <zhuyurun1993@gmail.com>
2018-10-16 11:56:38 +0800

    为buserole接口加上role

--- src/seed/api/endpoints/buserrole.py
+++ src/seed/api/endpoints/buserrole.py
@@ -1,8 +1,9 @@
-from flask import request
+from flask import request, g
 
 from seed.schema.base import BaseSchema
 from seed.api.endpoints._base import RestfulBaseView
 from seed.models.buserrole import BUserRole as BUserRoleModel
+from seed.models.role import Role as RoleModel
 from seed.utils.auth import api_require_login
 
 
@@ -21,6 +22,12 @@ class BUserRole(RestfulBaseView):
         query_session = self.session.query(self.model_class)
         datas = query_session.filter(self.model_class.user_id==model_id).all()
         datas = [row.row2dict() for row in datas] if datas else []
+
+        roles = RoleModel.get_roles(g.bussiness_id)
+        role_id_map = {role.id: role.role for role in roles}
+
+        for data in datas:
+            data['role'] = role_id_map[data['role_id']]
 
         return self.response_json(self.HttpErrorCode.SUCCESS, data=datas)
 
--- src/seed/models/role.py
+++ src/seed/models/role.py
@@ -5,3 +5,11 @@ __all__ = ['Role', ]
 
 class Role(BussinessModel):
     role = db.Column(db.Text, default='new')
+
+    @classmethod
+    def get_roles(cls, bussiness_id=None):
+        query_session = cls.query
+        if bussiness_id:
+            query_session = query_session.filter_by(bussiness_id=bussiness_id)
+
+        return query_session.all()
