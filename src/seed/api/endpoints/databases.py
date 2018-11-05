4021d6065e2b31a76c431f23dd34c990b104f51a
TomZhu <zhuyurun1993@gmail.com>
2018-11-05 15:52:32 +0800

    新增数据库修改接口

--- src/seed/api/endpoints/databases.py
+++ src/seed/api/endpoints/databases.py
@@ -24,3 +24,18 @@ class Databases(Buser):
         datas = query_session.filter(self.model_class.bussiness_id==bussiness_id).all()
         datas = [row.row2dict() for row in datas] if datas else []
         return self.response_json(self.HttpErrorCode.SUCCESS, data=datas)
+
+    def put(self, bussiness_id):
+        input_json = request.get_json()
+
+        schema = self.schema_class()
+        databases, errors = schema.load(input_json)
+        if errors:
+            return self.response_json(self.HttpErrorCode.PARAMS_VALID_ERROR, msg=errors)
+        databases.save()
+
+        common_batch_crud(DatabasesSchema, DatabasesModel, databases)
+
+        datas, errors = DatabasesSchema(many=True).dump(databases)
+
+        return self.response_json(self.HttpErrorCode.SUCCESS, data=datas)
