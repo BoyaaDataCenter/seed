11cd9edda86a7477ca6eb86b9050b81bb34e63b7
TomZhu <zhuyurun1993@gmail.com>
2018-11-16 09:55:58 +0800

    修改将多选传过来的List改成string“

--- src/seed/libs/data_access/dataware/base.py
+++ src/seed/libs/data_access/dataware/base.py
@@ -7,6 +7,10 @@ class DataModel(object):
         self.query = query
 
     def gen_sql(self):
+        for key, value in self.query.items():
+            if isinstance(value, list):
+                self.query[key] = '(%s)' % ', '.join([str(item) for item in value])
+
         return self.sql.format(**self.query)
 
     def query_data(self):
