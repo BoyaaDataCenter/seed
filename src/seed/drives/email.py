fedd2ef1cc23d54dd3e9a1c978b48924466ceb7f
zhuyurun <zhuyurun1993@gmail.com>
2018-11-26 16:31:32 +0800

    优化邮箱发送配置

--- src/seed/drives/email.py
+++ src/seed/drives/email.py
@@ -4,66 +4,33 @@ from email.header import Header
 from email.utils import formatdate
 
 
-email_conf = {
-    'smtpHost': 'mail.boyaa.com',
-    'smtpPort': '587',
-    'sslPort': 465,
-    'fromMail': 'd_alert@boyaa.com',
-    'username': 'd_alert',
-    'password': 'D@12!zy$34',
-}
+class Email(object):
+    def __init__(self, configs):
+        self.mail_backend = configs.MAIL_BACKEND
+        self.mail_host = configs.MAIL_HOST
+        self.mail_port = configs.MAIL_PORT
+        self.mail_user = configs.MAIL_USER
+        self.mail_password = configs.MAIL_PASSWORD
+        self.mail_use_tls = configs.MAIL_USE_TLS
+        self.mail_from = configs.MAIL_FROM
 
+    def send_mail(self, to_mail_list, title, message, message_type='plain', cc_mail_list=[]):
+        to_mails = '; '.join(to_mail_list)
+        cc_mails = '; '.join(cc_mail_list)
 
-def send_email(email_list, title, message, message_type='plain', cc_email_list=[]):
-    """
-    email_list list 邮件列表
-    title 标题
-    message 内容
-    message_type 内容类型
-    message_type = 'html', 'plain'
+        mime_text = MIMEText(message.encode('utf-8'), message_type, 'utf-8')
+        mime_text['Subject'] = Header(title, 'utf-8')
+        mime_text['From'] = self.mail_from
+        mime_text['To'] = to_mails
+        mime_text['Cc'] = cc_mails
+        mime_text['Date'] = formatdate()
 
-    通过email方式发送报警信息
-    #连接smtp服务器，明文/SSL/TLS三种方式，根据你使用的SMTP支持情况选择一种
-    #普通方式，通信过程不加密
-    # smtp = smtplib.SMTP(smtpHost,smtpPort)
-    # smtp.ehlo()
-    # smtp.login(username,password)
-    #-----------------------------------------------
-    #-----------------------------------------
-    #纯粹的ssl加密方式，通信过程加密，邮件数据安全
-    # smtp = smtplib.SMTP_SSL(smtpHost,sslPort)
-    # smtp.ehlo()
-    # smtp.login(username,password)
-    #-------------------------------------
-    #tls加密方式，通信过程加密，邮件数据安全，使用正常的smtp端口
-    """
-    result = True
-    if not email_list:
-        return result
-    try:
-        to_mail = ";".join(email_list)
-        cc_mail = ",".join(cc_email_list)
-        subject = title  # 邮件标题和内容
-        body = message
-        encoding = 'utf-8'  # 初始化邮件
-        mail = MIMEText(body.encode(encoding), message_type, encoding)
-        mail['Subject'] = Header(subject, encoding)
-        mail['From'] = email_conf['fromMail']
-        mail['To'] = to_mail
-        mail['Cc'] = cc_mail
-        mail['Date'] = formatdate()
-        smtp = smtplib.SMTP(email_conf['smtpHost'], email_conf['smtpPort'])
+        smtp = smtplib.SMTP(self.mail_host, self.mail_port)
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
-        smtp.login(email_conf['username'], email_conf['password'])
-        # 发送邮件
-        email_list.extend(cc_email_list)
-        smtp.sendmail(email_conf['fromMail'], email_list, mail.as_string())
-        smtp.close()
-        # print 'send mail succeed'
-    except Exception as e:
-        print(e)
-        result = False
+        smtp.login(self.mail_user, self.mail_password)
 
-    return result
+        smtp.sendmail(self.mail_from, to_mail_list+cc_mail_list, mime_text.as_string())
+
+        smtp.close()
--- src/seed/utils/mail.py
+++ src/seed/utils/mail.py
@@ -1,31 +1,31 @@
-from seed.drives.email import send_email
+from flask import current_app
+
+from seed.drives.email import Email
 
 
 def send_active_email(to_mail, active_url, redirect_url):
     """ 发送激活邮件
     """
     title = '新用户激活'
-    mail_context = """
+    mail_content = """
         亲爱的用户:
             感谢你使用Seed自定义报表系统，请点击以下链接激活用户:
             {active_url}?redirect_url={redirect_url}
             此链接24小时之后失效，请尽快激活邮箱。
         Seed团队
     """.format(active_url=active_url, redirect_url=redirect_url)
-    print(mail_context)
-    send_email([to_mail], title, mail_context)
+    Email(current_app.configs).send_mail([to_mail], title, mail_content)
 
 
 def send_reset_password_email(to_mail, active_url, redirect_url):
     """ 发送重置密码邮件
     """
     title = '密码重置'
-    mail_context = """
+    mail_content = """
         亲爱的用户:
             你的密码重置连接如下:
             {active_url}?redirect_url={redirect_url}
             此链接24小时之后失效，请尽快点击重置密码。
         Seed团队
     """.format(active_url=active_url, redirect_url=redirect_url)
-    print(mail_context)
-    send_email([to_mail], title, mail_context)
+    Email(current_app.configs).send_mail([to_mail], title, mail_content)
