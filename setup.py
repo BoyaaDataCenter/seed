5e3c1bc536605cf8993e4a307efcf47737720785
TomZhu <zhuyurun1993@gmail.com>
2018-12-03 15:44:57 +0800

    更新setup安装文件

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 
 VERSION = '0.1'
@@ -11,12 +12,30 @@ def get_requirements(env):
 install_requires = get_requirements('base')
 
 
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+    # for (path, directories, filenames) in os.walk(directory):
+    #     paths.append(os.path.join(path, '*'))
+    # return paths
+
+
+static_files = package_files(os.path.join('src', 'seed', 'static'))
+
+
 setup(
     name='seed',
     version=VERSION,
     install_requires=install_requires,
-    package_dir={'': 'src'},
     packages=find_packages('src'),
+    package_dir={'': 'src'},
+    # package_data={
+    #     '': static_folders
+    # },
+    data_files=static_files,
     include_package_data=True,
     entry_points={
         'console_scripts': [
