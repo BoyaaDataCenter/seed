963e097826fe7acb7c62540116ca1be4ab8f3968
ReoLei <ReoLei@boyaa.com>
2019-05-06 16:00:35 +0800

    新增:桑基图格式数据

---
+++ src/seed/libs/data_access/formatters/sankey.py
@@ -0,0 +1,26 @@
+import json
+from seed.libs.data_access.formatters.base import BaseFormatter
+
+
+class SankeyFormatter(BaseFormatter):
+    def __init__(self, *args, **kwargs):
+        super(SankeyFormatter, self).__init__(*args, **kwargs)
+
+    def format_data(self):
+        links = []
+        node_list = []
+        nodes = []
+        for key, value in self.data.items():
+            data = json.loads(key)
+            for _, v in data.items():
+                node_list.append(v)
+
+            data.update(value)
+            links.append(data)
+
+        node_list = list(set(node_list))
+
+        for n in node_list:
+            nodes.append({"name": n})
+            
+        return {"links": links, "nodes": nodes}
