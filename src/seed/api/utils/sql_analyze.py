6cdcc34ac1ac39055fe50bf03b6eb3b2ac9de130
ReoLei <ReoLei@boyaa.com>
2019-05-07 16:07:41 +0800

    修改:增加桑基图和地图格式对SQL字段的判断

--- src/seed/api/utils/sql_analyze.py
+++ src/seed/api/utils/sql_analyze.py
@@ -19,6 +19,7 @@ class SqlFieldAnalysis(RestfulBaseView):
         if 'sqls' not in input_json:
             return self.response_json(self.HttpErrorCode.PARAMS_VALID_ERROR, msg='获取SQL失败')
 
+        chartype = input_json.get("chartype")
         sqls = input_json['sqls']
         print("origin_sql:", sqls)
         sql_str = sqls.lower().replace('\n', ' ')
@@ -39,5 +40,17 @@ class SqlFieldAnalysis(RestfulBaseView):
                     fields.append(field_name)
             elif isinstance(token, Identifier):
                 break
+
+        sankey_fields = ["source", "target", "value"]
+        map_fields = ["region_name", "lat", "lng", "value"]
+
+        # 桑基图和地图SQL中必须包含指定字段
+        if chartype == "sankey":
+            if not set(sankey_fields).issubset(set(fields)):
+                return self.response_json(self.HttpErrorCode.PARAMS_VALID_ERROR, msg='SQL字段不符合要求')
+
+        elif chartype == "map":
+            if not set(map_fields).issubset(set(fields)):
+                return self.response_json(self.HttpErrorCode.PARAMS_VALID_ERROR, msg='SQL字段不符合要求')
 
         return self.response_json(self.HttpErrorCode.SUCCESS, data={'fields': fields})
